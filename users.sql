a9c3f2347cf0592af31f60f61c544a89115d9e9f
Murali Krishna <67138183+MuraliKrishna01@users.noreply.github.com>
2021-05-11 19:27:29 +0530

    for Internship

---
+++ users.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 08, 2018 at 09:24 AM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mywebsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `signup_date` date NOT NULL,
+  `profile_pic` varchar(255) NOT NULL,
+  `cover_pic` varchar(255) NOT NULL,
+  `num_posts` int(11) NOT NULL,
+  `num_likes` int(11) NOT NULL,
+  `user_closed` varchar(3) NOT NULL,
+  `friend_array` text NOT NULL,
+  `address` text NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `hometown` varchar(20) NOT NULL,
+  `country` varchar(20) NOT NULL,
+  `mobile` int(20) DEFAULT NULL,
+  `phone` int(20) DEFAULT NULL,
+  `work` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
